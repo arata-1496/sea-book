dd3b1ae8f5cbd3cb1b3993e20dbdb79626fa4c54
arata-1496 <ishiguro.arata@gmail.com>
2026-02-18 22:48:27 +0900

    正解のみを表示する

--- src/app/book/page.jsx
+++ src/app/book/page.jsx
@@ -25,6 +25,11 @@ export default function bookPage() {
   const { correctAnimals } = useCorrectAnimals();
   console.log("correctAnimals:", correctAnimals);
 
+  const checkList = correctAnimals.some(
+    (item) => item.animal_id === animals.animal_id,
+  );
+  console.log("checkList", checkList);
+
   if (loading) {
     return <div>読み込み中...</div>;
   }
@@ -34,19 +39,40 @@ export default function bookPage() {
       <h1 className="text-center font-black text-6xl mb-4">ずかん</h1>
       <div className="flex-1 overflow-y-auto">
         <div className="w-fit mx-auto  grid gap-3 grid-cols-3 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-5">
-          {animals.map((animal) => (
-            <div
-              key={animal.animal_id}
-              className="w-28 h-28 border-4 border-black  bg-yellow rounded-3xl flex justify-center items-center overflow-hidden p-2 "
-              onClick={() => handleAnimalDetail(animal)}
-            >
-              <img
-                src={animal.image}
-                alt={animal.name}
-                className="object-contain object-center max-w-24 max-h-24"
-              />
-            </div>
-          ))}
+          {animals.map((animal) => {
+            const isCorrect = correctAnimals.some(
+              (item) => item.animal_id === animal.animal_id,
+            );
+            console.log("isCorrect", isCorrect);
+            if (isCorrect) {
+              return (
+                <div
+                  key={animal.animal_id}
+                  className="w-28 h-28 border-4 border-black  bg-yellow rounded-3xl flex justify-center items-center overflow-hidden p-2 "
+                  onClick={() => handleAnimalDetail(animal)}
+                >
+                  <img
+                    src={animal.image}
+                    alt={animal.name}
+                    className="object-contain object-center max-w-24 max-h-24"
+                  />
+                </div>
+              );
+            } else {
+              return (
+                <div
+                  key={animal.animal_id}
+                  className="w-28 h-28 border-4 border-black  bg-yellow rounded-3xl flex justify-center items-center overflow-hidden p-2 "
+                >
+                  <img
+                    src="/hatena.png"
+                    alt="hatena"
+                    className="object-contain object-center max-w-24 max-h-24"
+                  />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
       <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
